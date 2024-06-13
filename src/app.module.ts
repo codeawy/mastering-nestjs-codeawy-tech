9ad1660f7a448abefdc26d7a7daeab2dbabd37d6
codeawy <codeawy@gmail.com>
2024-06-13 13:19:04 +0300

    new products resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [DashboardModule],
+  imports: [DashboardModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
